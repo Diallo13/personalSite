919c3cf3ec9f2dd0db4c9cbce85ba991cf27580d
Diallo13 <boubacar3013@outlook.com>
2018-08-03 10:49:54 -0700

    message

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-let Name = document.querySelector("#name");
-name.addEventListener("mouseover", savage);
+// let Name = document.querySelector("#name");
+// name.addEventListener("mouseover", savage);
 
-function savage(){
-    let element = document.getElementsByClassName("text");
-    element.id = "bug";
-    console.log("WTF");
+// function savage(){
+//     let element = document.getElementsByClassName("text");
+//     element.id = "bug";
+//     console.log("WTF");
     
     
-}
+// }
